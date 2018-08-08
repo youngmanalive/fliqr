52f9899832ab1a97530b1d4f1f70b87b862463d9
Nate Harris <nathanharris.sf@gmail.com>
2018-08-08 13:43:36 -0700

    Add errors, session errors, and entities reducer

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -2,7 +2,7 @@ import SessionApiUtil from '../util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
-
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
 export const signup = formUser => dispatch => (
   SessionApiUtil.signup(formUser).then(user => (
--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -2,6 +2,7 @@ import {
   RECEIVE_CURRENT_USER,
   LOGOUT_CURRENT_USER
 } from '../actions/session';
+import merge from 'lodash/merge';
 
 const _nullSession = {
   currentUser: null
@@ -11,7 +12,7 @@ export default (state = _nullSession, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
-      return Object.assign({}, { currentUser: action.user });
+      return merge({}, { currentUser: action.user });
     case LOGOUT_CURRENT_USER:
       return _nullSession;
     default:
