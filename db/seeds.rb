857855f3ced7d14404f58f2fed18b5afc4a1e047
Nate Harris <nathanharris.sf@gmail.com>
2018-08-10 15:15:38 -0700

    Add demo account seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+## Demo Account
+User.create(
+  fname: "Friend",
+  lname: "Account",
+  email: "demo@fliqr.com",
+  username: "demo",
+  password: "starwars"
+)
