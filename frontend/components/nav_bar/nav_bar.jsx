a6c3e3c30f337648d2c72806200fae3481ca1ea5
Nate Harris <nathanharris.sf@gmail.com>
2018-09-22 14:31:53 -0700

    Update navbar with avatar and info sprite

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -5,6 +5,26 @@ import SplashSessionContainer from '../splash/splash_session_container';
 const logo = <Link to='/' className='navbar-logo'>fliqr</Link>;
 
 
+// const userNav = (currentUser, logout) => (
+//   <div className='navbar-main-user'>
+//     {logo}
+//     <div className='navbar-user-links'>
+//       <Link to={`/users/${currentUser.id}`}>You</Link>
+//       <Link to='/explore' >Explore</Link>
+//     </div>
+//     <div className='navbar-user-info'>
+//       <Link to='/upload' className='navbar-upload-icon' />
+//       <div className='navbar-info-icon' />
+//       <h3 className='navbar-user-name'>Hello, {currentUser.fname}</h3>
+//       <button
+//         className='navbar-session-logout'
+//         onClick={() => logout()}>
+//         Log out
+//       </button>
+//     </div>
+//   </div>
+// );
+
 const userNav = (currentUser, logout) => (
   <div className='navbar-main-user'>
     {logo}
@@ -12,15 +32,30 @@ const userNav = (currentUser, logout) => (
       <Link to={`/users/${currentUser.id}`}>You</Link>
       <Link to='/explore' >Explore</Link>
     </div>
+
     <div className='navbar-user-info'>
       <Link to='/upload' className='navbar-upload-icon' />
-      <h3 className='navbar-user-name'>Hello, {currentUser.fname}</h3>
-      <button
-        className='navbar-session-logout'
-        onClick={() => logout()}>
-        Log out
-      </button>
+      <div className='navbar-info-icon' />
+
+      <div className='navbar-avatar-icon'>
+        <div className='navbar-session-dropdown-container'>
+          <div className='navbar-session-dropdown'>
+            <p className='navbar-user-name'>Hello, {currentUser.fname}!</p>
+            <p className='navbar-session-logout'
+              onClick={() => logout()}>
+              Sign Out
+              </p>
+            {/* <button
+              className='navbar-session-logout'
+              onClick={() => logout()}>
+              Log out
+            </button> */}
+          </div>
+        </div>
+      </div>
+
     </div>
+
   </div>
 );
 
