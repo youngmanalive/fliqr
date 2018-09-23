e167371a1a207e69a697f50ae118e6f0c2e1a1fa
Nate Harris <nathanharris.sf@gmail.com>
2018-09-22 17:51:29 -0700

    Compelete comments reducer

---
+++ frontend/reducers/comments_reducer.js
@@ -0,0 +1,25 @@
+import {
+  RECEIVE_ALL_COMMENTS,
+  RECEIVE_COMMENT,
+  REMOVE_COMMENT
+} from '../actions/comment_actions';
+import { LOGOUT_CURRENT_USER } from '../actions/session_actions';
+import merge from 'lodash/merge';
+
+export default (state = {}, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_ALL_COMMENTS:
+      return merge({}, action.comments);
+    case RECEIVE_COMMENT:
+      return merge({}, state, { [action.comment.id]: action.comment });
+    case REMOVE_COMMENT:
+      const newState = merge({}, state);
+      delete newState[action.commentId];
+      return newState;
+    case LOGOUT_CURRENT_USER:
+      return {};
+    default:
+      return state;
+  }
+}
