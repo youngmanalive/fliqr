ca100a3dd90780f5f0f43ab90470e8eae9305d74
Nate Harris <nathanharris.sf@gmail.com>
2018-09-22 17:11:20 -0700

    Complete comment controller actions

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,35 @@
+class Api::CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+
+    if @comment.save
+      render :show
+    else
+      render json: ['Comment error'], status: 422
+    end
+  end
+
+  def index
+    @comments = Comment.where(:photo_id = params[:photo_id])
+  end
+
+  def destroy
+    @comment = current_user.comments.find(params[:id])
+
+    if @comment
+      @comment.destroy
+    else
+      render json: ['Comment not found'], status: 404
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(
+      :user_id,
+      :photo_id,
+      :body
+    )
+  end
+end
