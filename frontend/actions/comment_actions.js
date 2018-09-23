7adf9da481994684c9b76ebeba97d80cca3831eb
Nate Harris <nathanharris.sf@gmail.com>
2018-09-22 17:38:57 -0700

    Complete comment actions

---
+++ frontend/actions/comment_actions.js
@@ -0,0 +1,32 @@
+import * as CommentApiUtil from '../util/comment_api_util';
+
+export const RECEIVE_ALL_COMMENTS = 'RECEIVE_ALL_COMMENTS';
+export const RECEIVE_COMMENT = 'RECEIVE_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export const RECEIVE_COMMENT_ERRORS = 'RECEIVE_COMMENT_ERRORS';
+
+const receiveAllComments = comments => ({ type: RECEIVE_ALL_COMMENTS, comments });
+const receiveComment = comment => ({ type: RECEIVE_COMMENT, comment });
+const removeComment = commentId => ({ type: REMOVE_COMMENT, commentId });
+const receiveErrors = errors => ({ type: RECEIVE_COMMENT_ERRORS, errors });
+
+export const fetchAllComments = id => dispatch => (
+  CommentApiUtil.fetchAllComments(id).then(
+    comments => dispatch(receiveAllComments(comments)),
+    err => dispatch(receiveErrors(err.responseJSON))
+  )
+);
+
+export const createComment = formData => dispatch => (
+  CommentApiUtil.createComment(formData).then(
+    comment => dispatch(receiveComment, comment),
+    err => dispatch(receiveErrors(err.responseJSON))
+  )
+);
+
+export const removeComment = commentId => dispatch => {
+  CommentApiUtil.deleteComment(commentId).then(
+    () => dispatch(removeComment(commentId)),
+    err => dispatch(receiveErrors(err.responseJSON))
+  )
+};
