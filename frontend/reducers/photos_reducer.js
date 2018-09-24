d1a333997f84ed762c416fe0526724c681a0491a
Nate Harris <nathanharris.sf@gmail.com>
2018-09-23 20:56:34 -0700

    Add comment id to photo state

--- frontend/reducers/photos_reducer.js
+++ frontend/reducers/photos_reducer.js
@@ -3,19 +3,23 @@ import {
   RECEIVE_PHOTO,
   REMOVE_PHOTO
 } from '../actions/photo_actions';
+import { RECEIVE_COMMENT, REMOVE_COMMENT } from '../actions/comment_actions';
 import { LOGOUT_CURRENT_USER } from '../actions/session_actions';
 import merge from 'lodash/merge';
 
 export default (state = {}, action) => {
   Object.freeze(state);
+  const newState = merge({}, state);
   switch (action.type) {
     case RECEIVE_ALL_PHOTOS:
       return merge({}, action.photos);
     case RECEIVE_PHOTO:
       return merge({}, state, { [action.photo.id]: action.photo });
     case REMOVE_PHOTO:
-      const newState = merge({}, state);
       delete newState[action.photoId];
+      return newState;
+    case RECEIVE_COMMENT:
+      newState[action.comment.photo_id].commentIds.push(action.comment.id);
       return newState;
     case LOGOUT_CURRENT_USER:
       return {};
