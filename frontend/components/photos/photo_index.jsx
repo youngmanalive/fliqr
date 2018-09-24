4f62c74150c7720dec9176c151d33e7701fdd526
Nate Harris <nathanharris.sf@gmail.com>
2018-09-24 14:29:20 -0700

    Update photo index with loading icon during fetch

--- frontend/components/photos/photo_index.jsx
+++ frontend/components/photos/photo_index.jsx
@@ -8,41 +8,52 @@ class PhotoIndex extends React.Component {
   }
 
   componentDidMount() {
-    this.props.fetchAllPhotos()
-      .then(() => this.setState({ loading: false }));
+    this.props.fetchAllPhotos();
+    setTimeout(() => this.setState({ loading: false }), 2500);
   }
 
-  shuffle(array) {
-    const shuffled = array.slice();
-    let current = shuffled.length, temp, random;
+  // shuffle(array) {
+  //   const shuffled = array.slice();
+  //   let current = shuffled.length, temp, random;
 
-    while (0 !== current) {
-      random = Math.floor(Math.random() * current);
-      current -= 1;
-      temp = shuffled[current];
-      shuffled[current] = shuffled[random];
-      shuffled[random] = temp;
-    }
+  //   while (0 !== current) {
+  //     random = Math.floor(Math.random() * current);
+  //     current -= 1;
+  //     temp = shuffled[current];
+  //     shuffled[current] = shuffled[random];
+  //     shuffled[random] = temp;
+  //   }
 
-    return shuffled;
+  //   return shuffled;
+  // }
+
+  loading() {
+    return (this.state.loading) ? (
+      <div className='loading-container'>
+        <div className='lds-ellipsis'>
+          <div></div><div></div><div></div><div></div>
+        </div>
+      </div>
+    ) : (null);
   }
 
   render() {
     const currentUserId = this.props.currentUserId;
 
-    return (this.state.loading) ? (
-      <h1 className='explore-page'>Loading...</h1>
-    ) : (
-      <div className='explore-page'>
-        <h1>Explore</h1>
-        <ul className='photo-index'>
-          {this.props.photos.map(photo => (
-            <PhotoIndexItem 
-              key={photo.id} 
-              photo={photo} 
-              currentUserId={currentUserId} />
-          ))}
-        </ul>
+    return (
+      <div>
+        {this.loading()}
+        <div className='explore-page'>
+          <h1>Explore</h1>
+          <ul className='photo-index'>
+            {this.props.photos.map(photo => (
+              <PhotoIndexItem 
+                key={photo.id} 
+                photo={photo} 
+                currentUserId={currentUserId} />
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
