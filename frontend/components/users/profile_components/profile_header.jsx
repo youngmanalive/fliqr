a87887723c4ba9a1007667cea2d9b5910de39df1
Nate Harris <nathanharris.sf@gmail.com>
2018-09-24 14:34:47 -0700

    Add gradient div to user profile header

--- frontend/components/users/profile_components/profile_header.jsx
+++ frontend/components/users/profile_components/profile_header.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
 
 const ProfileHeader = ({ user, photoCount }) => {
   const displayName = `${user.fname} ${user.lname}`.toLowerCase();
 
   return (
     <div className='user-profile-header'>
-
-      <div className='user-profile-header-main'>
-        <div className='user-profile-header-avatar'></div>
-        <div className='user-profile-header-info'>
-          <p className='user-profile-header-name'>{displayName}</p>
-          <p className='user-profile-header-username'>{user.username.toLowerCase()}</p>
+      <div className='user-profile-header-gradient'>
+        <div className='user-profile-header-main'>
+          <div className='user-profile-header-avatar'></div>
+          <div className='user-profile-header-info'>
+            <p className='user-profile-header-name'>{displayName}</p>
+            <p className='user-profile-header-username'>{user.username.toLowerCase()}</p>
+          </div>
+        </div>
+        <div className='user-profile-header-data'>
+          <span className='photo-count'>{photoCount} Photos</span>
+          <span className='joined-date'>Joined {user.created_at.slice(0, 4)}</span>
         </div>
       </div>
-
-      <div className='user-profile-header-data'>
-        <span className='photo-count'>{photoCount} Photos</span>
-        <span className='joined-date'>Joined {user.created_at.slice(0, 4)}</span>
-      </div>
-
     </div>
   );
 };
