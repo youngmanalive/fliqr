ab87d1fcfe0dd3c5441d1e9767b213895362e823
Nate Harris <nathanharris.sf@gmail.com>
2018-09-27 13:40:09 -0700

    Update album associations

--- app/models/album.rb
+++ app/models/album.rb
@@ -14,9 +14,8 @@ class Album < ApplicationRecord
   validates :user_id, :album_title, presence: true
 
   belongs_to :user
-  # has_many :album_photos, dependent: :destroy
-  # has_many :photos
-    # through: :album_photos
-    # source: :photo
-    
+  has_many :album_photos, dependent: :destroy
+  has_many :photos,
+    through: :album_photos,
+    source: :photo
 end
