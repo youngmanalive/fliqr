c7cf778651176b592dffcf15c5fd99a1a9e8f6fd
Nate Harris <nathanharris.sf@gmail.com>
2018-09-27 12:15:18 -0700

    Annotate ablum model and add user association

--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: albums
+#
+#  id                :bigint(8)        not null, primary key
+#  user_id           :integer          not null
+#  album_title       :string           not null
+#  album_description :string
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+
 require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
