393555c1c6437631abe125cc87eb20cb7d91ce0f
Nate Harris <nathanharris.sf@gmail.com>
2018-09-27 12:15:01 -0700

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_22_220631) do
+ActiveRecord::Schema.define(version: 2018_09_27_185448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 2018_09_22_220631) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "albums", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "album_title", null: false
+    t.string "album_description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_albums_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
