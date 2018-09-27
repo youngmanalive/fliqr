e070231c2cd0fa28d46750db0a2686824c26f6dd
Nate Harris <nathanharris.sf@gmail.com>
2018-09-27 12:14:22 -0700

    Annotate ablum model and add user association

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: albums
+#
+#  id                :bigint(8)        not null, primary key
+#  user_id           :integer          not null
+#  album_title       :string           not null
+#  album_description :string
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+
 class Album < ApplicationRecord
+  validates :user_id, :album_title, presence: true
+
+  belongs_to :user
 end
