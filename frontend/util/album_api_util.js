a16b927b95b24e85416203c3c6a557450b5e203f
Nate Harris <nathanharris.sf@gmail.com>
2018-09-27 13:40:34 -0700

    Add album api util

---
+++ frontend/util/album_api_util.js
@@ -0,0 +1,39 @@
+export const fetchAlbums = id => (
+  $.ajax({
+    method: 'GET',
+    url: `api/albums`,
+    data: { user_id: id }
+  })
+);
+
+export const fetchAlbum = id => (
+  $.ajax({
+    method: 'GET',
+    url: `api/albums/${id}`
+  })
+);
+
+export const createAlbum = formData => (
+  $.ajax({
+    method: 'POST',
+    url: `api/albums`,
+    data: formData,
+    processData: false,
+    contentType: false
+  })
+);
+
+export const updateAlbum = album => (
+  $.ajax({
+    method: 'PATCH',
+    url: `api/albums/${album.id}`,
+    data: { album }
+  })
+);
+
+export const deleteAlbum = id => (
+  $.ajax({
+    method: 'DELETE',
+    url: `api/albums/${id}`
+  })
+);
