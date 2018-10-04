b1bf274e18318b4ed4121dbbf1c7b46fa2670841
Nate Harris <nathanharris.sf@gmail.com>
2018-10-03 19:21:24 -0700

    Add album items to album index

--- frontend/components/albums/album_index.jsx
+++ frontend/components/albums/album_index.jsx
@@ -7,10 +7,30 @@ class AlbumIndex extends React.Component {
   }
 
   render() {
+    const { profileUserId, currentUserId, profileAlbums } = this.props;
+    const newAlbumButton = (profileUserId != currentUserId) ? (null) : (
+      <Link to={`/newalbum`}>New album</Link>
+    );
+
+    console.log(profileAlbums);
+
     return (
-      <div className='user-profile-album-index'>
-        <h1>A work in progress...</h1>
-        <Link to={`/newalbum`}>New album</Link>
+      <div className='user-profile-album-index-container'>
+        <div className='user-profile-album-index'>
+          <div className='album-index-header'>{newAlbumButton}</div>
+          <div className='album-index-list'>
+            {profileAlbums.map(album => (
+              <Link to={`${this.props.match.url}/albums/${album.id}`}
+                key={album.id}>
+                <div className='album-index-item'>
+                  {album.album_title}
+                  {album.photoIds.length}
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+        
       </div>
     );
   }
