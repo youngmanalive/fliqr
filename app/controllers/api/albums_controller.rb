893165339d59e6f6e4509d90645a5163481a3efb
Nate Harris <nathanharris.sf@gmail.com>
2018-10-03 19:09:17 -0700

    Add create, index, show to album controller

--- app/controllers/api/albums_controller.rb
+++ app/controllers/api/albums_controller.rb
@@ -1,11 +1,33 @@
 class Api::AlbumsController < ApplicationController
   def create
+    @album = Album.new(album_params)
+    @album.user_id = current_user.id
+    photo_ids = JSON.parse(params[:photo_ids])
+
+    if @album.save
+      photo_ids.each do |id|
+        AlbumPhoto.create(photo_id: id, album_id: @album.id)
+      end
+      render :show
+    else
+      render json: ['Error creating album'], status: 422
+    end
   end
 
   def index
+    id = params[:user_id]
+    @albums = Album.where(user_id: id)
+    render :index
   end
 
   def show
+    @album = Album.find(params[:id])
+    
+    if @album
+      render :show
+    else
+      render json: ['Album not found!'], status: 404
+    end
   end
 
   def update
