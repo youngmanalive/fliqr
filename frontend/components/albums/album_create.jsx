3cf549e1966f304558e2010018acfc80b94bf1b1
Nate Harris <nathanharris.sf@gmail.com>
2018-10-03 19:18:29 -0700

    Finish up album create component

--- frontend/components/albums/album_create.jsx
+++ frontend/components/albums/album_create.jsx
@@ -6,42 +6,126 @@ class AlbumCreate extends React.Component {
     this.state = {
       album_title: '',
       album_description: '',
-      photo_ids: []
+      photo_ids: {},
+      loading: true
     };
   }
 
   componentDidMount() {
-    console.log('album create has mounted');
+    this.props.fetchAllPhotos(this.props.currentUserId)
+      .then(this.setState({ loading: false }));
   }
 
   clearSelection() {
-    this.setState({ photo_ids: [] });
+    document.querySelectorAll('.album-create-photo-item').forEach(el => {
+      el.style.border = '3px solid transparent';
+    });
+    this.title.value = '';
+    this.description.value = '';
+
+    this.setState({
+      album_title: '',
+      album_description: '',
+      photo_ids: {}
+    });
+  }
+
+  handleCancel(e) {
+    e.preventDefault();
+    if (confirm('Are you sure?')) {
+      this.props.history.push(`/users/${this.props.currentUserId}/albums`);
+    }
   }
 
   handleUpdate(field) {
     return e => this.setState({ [field]: e.target.value });
   }
 
+  handleSelect(e, id) {
+    const photo_ids = this.state.photo_ids;
+    if (photo_ids[id]) {
+      delete photo_ids[id];
+      e.currentTarget.style.border = '3px solid transparent';
+    } else {
+      photo_ids[id] = id;
+      e.currentTarget.style.border = '3px solid red';
+    }
+    this.setState({ photo_ids });
+  }
+
   handleSubmit(e) {
     e.preventDefault();
-    window.alert("Hold up, album upload is not implemented yet! :)");
+    const formData = new FormData();
+    formData.append('album[album_title]', this.state.album_title);
+    formData.append('album[album_description]', this.state.album_description);
+    formData.append('photo_ids', JSON.stringify(
+      Object.values(this.state.photo_ids)
+    ));
+
+    this.props.createAlbum(formData)
+      .then(() => this.props.history.push(
+        `/users/${this.props.currentUserId}/albums`
+      ));
+  }
+
+  loading() {
+    return (
+      <div className='loading-container'>
+        <div className='lds-ellipsis'>
+          <div></div><div></div><div></div><div></div>
+        </div>
+      </div>
+    );
   }
 
   render() {
+    if (this.state.loading) return this.loading();
+
+    const { photos } = this.props;
+    const submitReady = !this.state.album_title || !Object.keys(this.state.photo_ids).length;
+    const revertChanges = (!this.state.album_title && !Object.keys(this.state.photo_ids).length) ?
+      (null) : (<span 
+                  onClick={this.clearSelection.bind(this)} 
+                  className='album-create-clear'>Revert changes
+                </span>);
+
     return (
     <div className='album-create-container'>
-      <form onSubmit={this.handleSubmit.bind(this)}>
-        <input 
-          type='text' 
-          className='album-create-input-title' 
-          placeholder='Add album title'
-          onChange={this.handleUpdate('album_title')} />
-        <textarea
-          className='album-create-input-description'
-          placeholder='Album description'
-          onChange={this.handleUpdate('album_description')} />
-        <button type='submit'>Save</button>
-      </form>
+      <div className='album-create-form'>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <input 
+            type='text'
+            ref={el => { this.title = el; }}
+            className='album-create-input-title' 
+            placeholder='album title'
+            onChange={this.handleUpdate('album_title')} />
+          <textarea
+            ref={el => { this.description = el; }}
+            className='album-create-input-description'
+            placeholder='album description'
+            onChange={this.handleUpdate('album_description')} />
+          <button 
+            type='submit'
+            className='album-create-submit'
+            disabled={submitReady}>SAVE</button>
+          <button
+            className='album-create-cancel'
+            onClick={this.handleCancel.bind(this)}>CANCEL</button>
+          {revertChanges}
+        </form>
+      </div>
+      <div className='album-create-photo-select-container'>
+        <div className='album-create-photo-select'>
+          {photos.map(photo => (
+            <div 
+              key={photo.id}
+              className='album-create-photo-item' 
+              onClick={(e) => this.handleSelect(e, photo.id)}>
+              <img src={photo.photoUrl} />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
     );
   }
