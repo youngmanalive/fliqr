ba7bf0dacc99dc2520ae61f9f1c6a683f1defc0e
Nate Harris <nathanharris.sf@gmail.com>
2018-10-03 19:16:54 -0700

    Add albums to entities reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
 import photosReducer from './photos_reducer';
 import commentsReducer from './comments_reducer';
+import albumsReducer from './albums_reducer';
 
 export default combineReducers({
   users: usersReducer,
   photos: photosReducer,
-  comments: commentsReducer
+  comments: commentsReducer,
+  albums: albumsReducer
 });
