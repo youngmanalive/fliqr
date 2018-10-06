9e73002054c75ecd5a193ed2a62050d8ac1b8bc6
Nate Harris <nathanharris.sf@gmail.com>
2018-10-05 22:35:25 -0700

    Add user and album errors to errors reducer

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -2,9 +2,13 @@ import { combineReducers } from 'redux';
 import sessionErrorsReducer from './session_errors_reducer';
 import photoErrorsReducer from './photo_errors_reducer';
 import commentErrorsReducer from './comment_errors_reducer';
+import userErrorsReducer from './user_errors_reducer';
+import albumErrorsReducer from './album_errors_reducer';
 
 export default combineReducers({
   session: sessionErrorsReducer,
+  users: userErrorsReducer,
   photos: photoErrorsReducer,
+  albums: albumErrorsReducer,
   comments: commentErrorsReducer
 });
