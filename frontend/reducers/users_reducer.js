4a01e045e4cc1288f9a6bc0eb58745501aa6f963
Nate Harris <nathanharris.sf@gmail.com>
2018-10-05 22:36:28 -0700

    Clean up

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,4 +1,7 @@
-import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from '../actions/session_actions';
+import {
+  RECEIVE_CURRENT_USER,
+  LOGOUT_CURRENT_USER
+} from '../actions/session_actions';
 import { RECEIVE_USER } from '../actions/user_actions';
 import merge from 'lodash/merge';
 
