c7ffab1f9f05bd2d166ac5ef233232ca53a204c0
Nate Harris <nathanharris.sf@gmail.com>
2018-10-06 18:47:20 -0700

    Change album index to functional component

--- frontend/components/albums/album_index.jsx
+++ frontend/components/albums/album_index.jsx
@@ -1,38 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-class AlbumIndex extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const AlbumIndex = (props) => {
+  const { profileUserId, currentUserId, profileAlbums, deleteAlbum } = props;
+  const albumArray = Object.values(profileAlbums);
+  const newAlbumButton = (profileUserId != currentUserId) ? (null) : (
+    <Link to={`/organize`}>New album</Link>
+  );
+  const deleteButton = (profileUserId != currentUserId) ? (null) : (
+    <div className='album-index-item-delete'>
+      Delete
+    </div>
+  );
 
-  render() {
-    const { profileUserId, currentUserId, profileAlbums } = this.props;
-    const albumArray = Object.values(profileAlbums);
-    const newAlbumButton = (profileUserId != currentUserId) ? (null) : (
-      <Link to={`/organize`}>New album</Link>
-    );
-
-    return (
-      <div className='user-profile-album-index-container'>
-        <div className='user-profile-album-index'>
-          <div className='album-index-header'>{newAlbumButton}</div>
-          <div className='album-index-list'>
-            {albumArray.map(album => (
-              <Link to={`${this.props.match.url}/albums/${album.id}`}
-                key={album.id}>
-                <div className='album-index-item'>
-                  {album.album_title}
-                  {album.photoIds.length}
-                </div>
-              </Link>
-            ))}
-          </div>
+  return (
+    <div className='user-profile-album-index-container'>
+      <div className='user-profile-album-index'>
+        <div className='album-index-header'>{newAlbumButton}</div>
+        <div className='album-index-list'>
+          {albumArray.map(album => (
+            <Link to={`${props.match.url}/albums/${album.id}`}
+              key={album.id}>
+              <div className='album-index-item'>
+                <div className='album-title'>{album.album_title}</div>
+                <div className='album-photo-count'>{album.photoIds.length} photos</div>
+              </div>
+            </Link>
+          ))}
         </div>
-        
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default AlbumIndex;
