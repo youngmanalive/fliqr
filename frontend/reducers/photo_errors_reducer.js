fdd2e7d0669d5987e263bf1ba9d05523d88cb287
Nate Harris <nathanharris.sf@gmail.com>
2019-01-14 20:13:27 -0800

    Remove reducer for heroku build

--- frontend/reducers/photo_errors_reducer.js
+++ frontend/reducers/photo_errors_reducer.js
@@ -2,7 +2,6 @@ import {
   RECEIVE_PHOTO_ERRORS, 
   RECEIVE_PHOTO 
 } from '../actions/photo_actions';
-import { CLEAR_PHOTO_ERRORS } from '../actions/error_actions';
 
 export default (state = {}, action) => {
   Object.freeze(state);
